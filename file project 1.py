148c3b3095af6520d7245753280eb422b565f51b
enjoy-hash <86073293+enjoy-hash@users.noreply.github.com>
2021-06-18 00:22:27 +0530

    Radius and file extension project
    This link has code for the radius of a circle project and to print the file extension name

---
+++ file project 1.py
@@ -0,0 +1,6 @@
+r=str(input("Input the filename: "))
+x="Java"
+y="Pyhton"
+if (".py" in r):
+    print("The extension of file is: ",y)
+
---
+++ radius project 1.py
@@ -0,0 +1,3 @@
+r=float(input("Input the Radius of the circle: "))
+p=3.14
+print("The area of the circle with radius ", r, "is: ", p*r**2)

99d80d8d037558312bf33e3e6d6e589bda3404f6
enjoy-hash <86073293+enjoy-hash@users.noreply.github.com>
2021-06-22 18:50:39 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,10 @@
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+print("List 1")
+for number in list1:
+    if number > 0:
+        print(number)
+print("List 2")
+for number in list2:
+    if number > 0:
+        print(number)
